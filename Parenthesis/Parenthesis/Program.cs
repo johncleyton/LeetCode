96e5b8e4bb5c539c7decf92bc3233f20b12630ff
johncleyton <fredscholiveira@uol.com.br>
2023-11-04 19:09:48 -0300

    Added TwoSum and ParethensisCheck

---
+++ Parenthesis/Parenthesis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public static void Main()
+    {
+        Solution s = new Solution();
+        Console.WriteLine(s.IsValid("()[]{}"));
+    }
+    public bool IsValid(string s)
+    {
+        if (s.Length <= 1)
+            return false;
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s)
+        {
+            if (c == '(')
+                stack.Push(')');
+            else if (c == '{')
+                stack.Push('}');
+            else if (c == '[')
+                stack.Push(']');
+            else if (stack.Count == 0 || stack.Pop() != c)
+                return false;
+        }
+        return stack.Count == 0;
+    }
+}
---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Versioning;
+
+public class Solution
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        int[] nums = { 3, 2, 4 };
+        Console.WriteLine(solution.TwoSum(nums, 6));
+    }
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] result = new int[2];
+        for (int i = 0; i <= nums.Length; i++)
+        {
+            int aux = target - nums[i];
+            int index = Array.IndexOf(nums, aux);
+            if (index != -1 && index != i)
+            {
+                result[0] = i;
+                result[1] = index;
+                break;
+            }
+        }
+        return result;
+    }
+}
